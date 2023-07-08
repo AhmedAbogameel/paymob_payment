20847a5a772750b0c723ce5503f4ead41e9971b5
AhmedAbogameel <gamilco81@gmail.com>
2023-07-08 16:49:00 +0300

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:paymob_payment/paymob_payment.dart';
 
 void main() {
+  // Testing info do not use in your app
   PaymobPayment.instance.initialize(
     apiKey: "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8yT1RBMk56VjkuX3lZTklHVGkwVXBLTmVzdjh0Q0dxc0ZlTmVkNnJobGFid2RoUXlJNFFuMlBUZ2k1Q3VyaGFxbmt2SGlrbXo1enBzaUdxYmhiU0pDU3VCTzA4bGxpcFE=",
     integrationID: 3364826,
@@ -14,8 +15,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PaymentView(),
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF007aec),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(const Color(0xFF007aec)),
+          )
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const PaymentView(),
     );
   }
 }
@@ -34,14 +46,15 @@ class _PaymentViewState extends State<PaymentView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Paymob Payment'),
+        title: const Text('Paymob'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Image.network('https://paymob.com/images/logoC.png'),
+            const SizedBox(height: 24),
             if (response != null)
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -57,14 +70,14 @@ class _PaymentViewState extends State<PaymentView> {
                 ],
               ),
             ElevatedButton(
-              child: const Text('Pay'),
+              child: const Text('Pay for 200 EGP'),
               onPressed: () => PaymobPayment.instance.pay(
                 context: context,
                 currency: "EGP",
-                amountInCents: "20000", // 200 EGP
+                amountInCents: "20000",
                 onPayment: (response) => setState(() => this.response = response),
               ),
-            ),
+            )
           ],
         ),
       ),
